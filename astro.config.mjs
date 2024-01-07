9ec767dca365ce0561c3e9d9876622df1a99f03e
David Blanch Bellido <dablabe98@gmail.com>
2024-01-07 20:15:50 +0100

    Add web analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,11 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
   output: 'server',
   adapter: vercel({
     webAnalytics: {
       enabled: true,
     },
   }),
+  integrations: [tailwind()]
 });
